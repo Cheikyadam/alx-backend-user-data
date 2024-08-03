a9ac1cfef7e17f1559d6e0e6846fd4049166ecec
Cheikyadam <urbaingande@gmail.com>
2024-08-03 02:13:25 +0100

    0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 """filtering logger data"""
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator) -> str:
+def filter_datum(
+        fields: List(str),
+        redaction: str, message: str, separator: str) -> str:
     """function def here"""
     for field in fields:
         message = re.sub(rf'{field}=[^;]+', f"{field}={redaction}", message)
