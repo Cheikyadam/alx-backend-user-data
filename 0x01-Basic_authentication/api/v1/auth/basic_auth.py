ac247e0b76a7dc1977b8dc034d3b5c7f3695b077
Cheikyadam <urbaingande@gmail.com>
2024-08-07 17:28:46 +0100

    8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """basic auth class"""
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -15,3 +16,17 @@ class BasicAuth(Auth):
         if authorization_header.startswith("Basic ") is False:
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """decode base 64 header"""
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            value = base64.b64decode(
+                    base64_authorization_header, validate=True)
+            return value.decode('utf-8')
+        except Exception:
+            return None
