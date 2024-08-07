56631aea2584a7014fb2f465271a1e1a1a3faae1
Cheikyadam <urbaingande@gmail.com>
2024-08-07 15:51:12 +0100

    5

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,7 +11,15 @@ class Auth:
     """auth class def and methodes"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """require func"""
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path[-1:] != "/":
+            path = path + "/"
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """auth funct"""
