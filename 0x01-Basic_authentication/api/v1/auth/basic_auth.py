6b752581750d4f1db2fb43885a230953e2493231
Cheikyadam <urbaingande@gmail.com>
2024-08-08 08:07:32 +0100

    10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,8 @@
 """basic auth class"""
 from api.v1.auth.auth import Auth
 import base64
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -43,3 +45,17 @@ class BasicAuth(Auth):
         cred = decoded_base64_authorization_header.split(':')
         if len(cred) == 2:
             return (cred[0], cred[1])
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """get connected user"""
+        if user_email is None or type(user_email) is not str:
+            return None
+        if user_pwd is None or type(user_pwd) is not str:
+            return None
+        res = (User.search({'email': user_email}))
+        if len(res) == 0:
+            return None
+        if res[0].is_valid_password(user_pwd):
+            return res[0]
+        return None
