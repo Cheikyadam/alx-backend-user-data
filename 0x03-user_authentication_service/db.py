9a6cbae70e28818e0806436f5f44d8645044cb0a
Cheikyadam <urbaingande@gmail.com>
2024-08-13 20:25:56 +0100

    db

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -42,7 +42,7 @@ class DB:
         self._session.commit()
         return user
 
-    def find_user_by(self, **kwargs: Dict) -> User:
+    def find_user_by(self, **kwargs) -> User:
         """find user by"""
         try:
             user = self._session.query(User).filter_by(**kwargs).one()
