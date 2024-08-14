b98c040dff9b548a7ea57e5616fd555798be87d3
Cheikyadam <urbaingande@gmail.com>
2024-08-14 12:30:55 +0100

    11

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """flask app"""
-from flask import Flask, jsonify, request
+from flask import Flask, abort, jsonify, request
 from auth import Auth
 
 app = Flask(__name__)
@@ -27,6 +27,23 @@ def users() -> str:
     except Exception as e:
         print(f"error: {e}")
         return jsonify({"error": "wrong format"}), 404
+
+
+@app.route("/sessions", methods=["POST"])
+def login() -> str:
+    """login func"""
+    try:
+        email = request.form.get("email")
+        pwd = request.form.get("password")
+        if auth.valid_login(email, pwd):
+            session_id = auth.create_session(email)
+            out = jsonify({"email": email, "message": "logged in"})
+            out.set_cookie("session_id", session_id)
+            return out
+        else:
+            abort(401)
+    except Exception as e:
+        abort(401)
 
 
 if __name__ == "__main__":
