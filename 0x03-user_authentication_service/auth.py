7b0c6bb4ce8871d330e7f02b34b887348f417d16
Cheikyadam <urbaingande@gmail.com>
2024-08-14 13:02:31 +0100

    auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -61,3 +61,18 @@ class Auth:
             return session_id
         except Exception:
             pass
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """get user from session"""
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except Exception as e:
+            return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """destroy session"""
+        my_dict = {"session_id": None}
+        self._db.update_user(user.id, **my_dict)
